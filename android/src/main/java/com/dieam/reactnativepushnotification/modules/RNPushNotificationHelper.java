d9a9d64e3ec4c52d01cc3117ab840398873554e2
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2019-09-25 16:25:28 +0200

    Support for circled large icon

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -14,7 +14,12 @@ import android.content.pm.ApplicationInfo;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -157,6 +162,12 @@ public class RNPushNotificationHelper {
             Log.v(LOG_TAG, "Setting largeIcon from value " + largeIcon);
 
             Bitmap largeIconBitmap = getBitmap(largeIcon);
+
+            String largeIconStyle = bundle.getString("largeIconStyle");
+
+            if(largeIconStyle == "circle")
+                largeIconBitmap = getCircleBitmap(largeIconBitmap);
+
             return largeIconBitmap;
         }
 
@@ -454,13 +465,53 @@ public class RNPushNotificationHelper {
                 connection.setDoInput(true);
                 connection.connect();
                 InputStream input = connection.getInputStream();
-                Bitmap myBitmap = BitmapFactory.decodeStream(input);
-                return myBitmap;
+                Bitmap decodedBitmap = BitmapFactory.decodeStream(input);
+                return decodedBitmap;
             } catch (IOException e) {
                 Log.v(LOG_TAG, "Failed to load image from url: " + strURL);
                 e.printStackTrace();
                 return null;
             }
+        }
+
+        private Bitmap getCircleBitmap(Bitmap bitmap) {
+            Bitmap output;
+            Rect srcRect, dstRect;
+            float r;
+            final int width = bitmap.getWidth();
+            final int height = bitmap.getHeight();
+
+            if (width > height){
+                output = Bitmap.createBitmap(height, height, Bitmap.Config.ARGB_8888);
+                int left = (width - height) / 2;
+                int right = left + height;
+                srcRect = new Rect(left, 0, right, height);
+                dstRect = new Rect(0, 0, height, height);
+                r = height / 2;
+            }else{
+                output = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+                int top = (height - width)/2;
+                int bottom = top + width;
+                srcRect = new Rect(0, top, width, bottom);
+                dstRect = new Rect(0, 0, width, width);
+                r = width / 2;
+            }
+
+            Canvas canvas = new Canvas(output);
+
+            final int color = 0xff424242;
+            final Paint paint = new Paint();
+
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(color);
+            canvas.drawCircle(r, r, r, paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(bitmap, srcRect, dstRect, paint);
+
+            bitmap.recycle();
+
+            return output;
         }
     }
 
